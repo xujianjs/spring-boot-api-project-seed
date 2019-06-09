a9678900576f505a848be89b8dc2df9ed63f1006
xujian <690378622@qq.com>
2019-06-09 11:56:04 +0800

    springboot添加mock测试

---
+++ src/test/java/MockTest.java
@@ -0,0 +1,53 @@
+import com.company.project.Application;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static sun.nio.cs.Surrogate.is;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class MockTest {
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();  //构造MockMvc
+    }
+
+    @Test
+    public void test() throws Exception {
+        mvc.perform(post("/user/detail") // //调用接口
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .param("id", "1")//传入添加的用户参数 
+
+                .accept(MediaType.APPLICATION_JSON))  //接收的类型
+                .andExpect(status().isOk())   //判断接收到的状态是否是200
+                .andDo(print())  //打印内容
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().string(Matchers.containsString("200")))
+        ; //匹配返回值中的内容
+
+    }
+
+
+
+
+
+}
